364e91f9eb8c35776f8fe09f14efb6d7e42a6b12
vespasiano <vespasianomatheus.gmail.com>
2025-01-27 19:55:05 -0300

    update no serviço de criação de transações

---
+++ src/services/CreateTransacao.service.ts
@@ -0,0 +1,50 @@
+import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
+import timezone from 'dayjs/plugin/timezone'
+
+import {
+  ITransacaoRepository,
+  Transacao,
+} from '../repositories/interface/ITransacaoRepository'
+import { UnprocessableEntity } from './errors/unprocessableEntity'
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
+
+interface CreateTransacaoRequest {
+  valor: number
+  dataHora: Date
+}
+
+interface CreateTransacaoServiceResponse {
+  transacao: Transacao
+}
+
+export class CreateTransacaoService {
+  constructor(private transacaoRepository: ITransacaoRepository) {}
+
+  async execute({
+    valor,
+    dataHora,
+  }: CreateTransacaoRequest): Promise<CreateTransacaoServiceResponse> {
+    const isPositive = valor >= 0
+
+    const dataHoraRequest = dayjs(new Date(dataHora))
+    const currentDateTime = dayjs(new Date()).utc(true)
+
+    const isDateAfterCurrentDate = dataHoraRequest.isAfter(currentDateTime)
+
+    if (!isPositive || isDateAfterCurrentDate) {
+      throw new UnprocessableEntity()
+    }
+
+    const transacao = await this.transacaoRepository.create({
+      valor,
+      dataHora,
+    })
+
+    return {
+      transacao,
+    }
+  }
+}
---
+++ src/services/createTransacao.service.spec.ts
@@ -0,0 +1,45 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { ImMemoryTransacaoRepository } from '../repositories/inMemoryTransacao.repository'
+import { CreateTransacaoService } from './CreateTransacao.service'
+import { UnprocessableEntity } from './errors/unprocessableEntity'
+
+import dayjs from 'dayjs'
+
+let transacaoRepository: ImMemoryTransacaoRepository
+let sut: CreateTransacaoService
+
+describe('Transacao Service', () => {
+  beforeEach(() => {
+    transacaoRepository = new ImMemoryTransacaoRepository()
+    sut = new CreateTransacaoService(transacaoRepository)
+  })
+
+  it('should  be able to send a transaction', async () => {
+    const { transacao } = await sut.execute({
+      valor: 2002,
+      dataHora: new Date('2025-01-26T18:30:00Z'),
+    })
+
+    expect(transacao.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to send a transaction with a negative value', async () => {
+    await expect(() =>
+      sut.execute({
+        valor: -1,
+        dataHora: new Date(),
+      }),
+    ).rejects.toBeInstanceOf(UnprocessableEntity)
+  })
+
+  it('should not be able to send a transaction with a datetime greater than the current', async () => {
+    const futureDateTime = dayjs(new Date()).add(1, 'second').toDate()
+
+    await expect(() =>
+      sut.execute({
+        valor: 2002,
+        dataHora: futureDateTime,
+      }),
+    ).rejects.toBeInstanceOf(UnprocessableEntity)
+  })
+})
