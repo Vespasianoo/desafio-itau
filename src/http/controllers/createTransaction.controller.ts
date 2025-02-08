4ad2a00fe2eb5234bbfdd162e53072948e547e28
vespasiano <vespasianomatheus@gmail.com>
2025-02-08 10:57:16 -0300

    adicionando a validação da data (ISO 8601)

--- src/http/controllers/createTransaction.controller.ts
+++ src/http/controllers/createTransaction.controller.ts
@@ -11,10 +11,16 @@ export async function createTransactionController(
 ) {
   const createTransactionBodySchema = z.object({
     valor: z.number(),
-    dataHora: z.coerce.date(),
+    dataHora: z
+      .string()
+      .refine((val) => {
+        const iso8601Regex =
+          /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{3})?(Z|([+\\-]\d{2}:\d{2}))$/
+        return iso8601Regex.test(val) // Validação da data no formato ISO 8601
+      })
+      .transform((val) => new Date(val)), // Converte a string para um objeto Date
   })
   const { valor, dataHora } = createTransactionBodySchema.parse(request.body)
-
   try {
     const createTransactionService = new CreateTransactionService(
       transactionRepository,
